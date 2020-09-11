3a8d6919498de722d91ccb0bd089cb27675f6b73
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-09-11 13:22:50 -0700

    added function to find street direction

--- la_2.py
+++ la_2.py
@@ -1,4 +1,8 @@
-street = int(input("Put the street number wish to find the direction of here: "))
-
 def street_direction(street):
-    if
+    if (street % 2 == 0):
+        print("This street is eastbound.")
+    else:
+        print("This street is westbound.")
+
+street = int(input("Put the street number wish to find the direction of here: "))
+street_direction(street)
